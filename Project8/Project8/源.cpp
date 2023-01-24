31fbd4d4ea691b03cf864177379e98fc696637c5
wj729318 <326738373@qq.com>
2023-01-24 23:01:49 +0800

    5，指针
    初始系列5和指针

--- Project8/Project8/源.cpp
+++ Project8/Project8/源.cpp
@@ -75,17 +75,599 @@
 #include<stdlib.h>
 #include<Windows.h>
 #include <stdio.h>
+#include <cassert>
+#include <stdio.h>
 int main()
 {
 	int n = 9;
-	float* pFloat = (float*)&n;
-	printf("n的值为：%d\n", n);
-	printf("*pFloat的值为：%f\n", *pFloat);
-	*pFloat = 9.0;
-	printf("num的值为：%d\n", n);
-	printf("*pFloat的值为：%f\n", *pFloat);
+	int x = 0;
+	int y = 0, a;
+	//scanf("%d", n);
+	for (y = 2*n; y > 0; y -=1)
+	{
+		for (x =0; x <2*n; x+=1)
+		{
+			char ch = (y - 2 * x)>0 ? '!': '*';
+			putchar(ch);
+		}
+		printf("\n");
+	}
 	return 0;
 }
+
+
+
+
+//*
+//**
+//****
+//*****
+//******
+//*******
+//********
+//*********
+//**********
+//void print(int* p,int i)
+//{
+//
+//	/*while (i--)
+//	{
+//		printf("%d ", *p++);
+//	}*/
+//	/*int j = 0;
+//	for(j=0;j<i;j++)
+//	printf("%d %d ", p[j],  *(p + j));*/
+//}
+//int main()
+//{
+//	int shu[] = { 1,2,3,54,67,74 };
+//	int i=sizeof(shu) / sizeof(shu[0]);
+//	print(shu,i);
+//	return 0;
+//}
+//void reverse( char* p,int n)
+//{
+//    char* left=p;
+//	char* right = p + n - 2;
+//	char temp;
+//	
+//	while (left<right)
+//	{
+//		temp= *left;
+//		*left = *right;
+//		*right = temp;
+//		left++;
+//		right--;
+//	}
+//	printf("%s\n", p);
+//}
+//int main()
+//{
+//	char shu[] = "abcdefg";
+//	int n = sizeof(shu) / sizeof(shu[0]);
+//	reverse(shu,n);
+//	printf("%s", shu);
+//	return 0;
+//}
+//void reverse(char* p)
+//{
+//
+//	char* temp=p;
+//	char c ;
+//	while (*temp++!= '\0')
+//	{
+//		;
+//	}
+//	temp = temp - 2;
+//	while (p<temp)
+//	{
+//		c = *p;
+//		*p = *temp;
+//		*temp  = c;
+//		p++;
+//		temp--;
+//	}
+//}
+//int main()
+//{
+//	char shu[] = "abcdefg";
+//	reverse(shu);
+//	printf("%s",shu);
+//	return 0;
+//}
+//int compar(const void* x, const void* y)
+//{
+//	return (*(int*)x - *(int*)y);
+//}
+//void swap(void* x, void* y,int size)
+//{
+//	int i = 0;
+//	for (i = 0; i < size; i++)
+//	{
+//		char temp = *((char*)x + i);
+//		*((char*)x+i )= *((char*)y+i);
+//		*((char*)y+i) = temp;
+//	}
+//	
+//
+//}
+//void bubble(void* shu, int num, int size, int(*compar)(const void* ,const void* ))
+//{
+//	int i = 0;
+//	int j = 0;
+//	for (i = 0; i < num; i++)
+//	{
+//		for (j = 0; j < num - i - 1; j++)
+//		{
+//			if (compar((char*)shu+j*size,(char*)shu+(j+1)*size)> 0)
+//			{
+//				swap((char*)shu + j * size, (char*)shu + (j + 1) * size, size);
+//
+//			}
+//		}
+//		
+//	}
+//}
+//
+//int main()
+//{
+//	int i = 0;
+//	int shu[] = {11,33,77,3,5,1,11,99,79,31,57 };
+//	int num = sizeof(shu) / sizeof(shu[0]);
+//	//qsort(shu,num, sizeof(shu[0]),compar);
+//	//具体的qsort函数的实现，我们用冒泡的思想去模拟一下：
+//	bubble(shu,num , sizeof(shu[0]), compar);
+//	for (i = 0; i < num; i++)
+//	{
+//		printf("%d ", shu[i]);
+//	}
+//	return 0;
+//}
+
+//void manu()
+//{
+//	printf("********************************\n");
+//	printf("******1、加法    2、减法********\n");
+//	printf("********************************\n");
+//	printf("******3、乘法    4、除法********\n");
+//	printf("********************************\n");
+//	printf("**********0、退出 **************\n");
+//
+//}
+//int add(int a, int b)
+//{
+//	return (a + b);
+//}
+//int sub(int a, int b)
+//{
+//	return (a - b);
+//}
+//int mul(int a, int b)
+//{
+//	return a * b;
+//}
+//int dev(int a, int b)
+//{
+//	return a / b;
+//}
+////void cala(int(*(*p)[5])(int , int ),int input)
+////{
+////	int a = 0;
+////	int b = 0;
+////	
+////	printf("请输入两个数：");
+////	scanf("%d %d", &a, &b);
+////	printf("%d\n",(*p)[input](a, b));
+////}
+//void cala1(int(*p)(int, int))
+//{
+//	int a = 0;
+//	int b = 0;
+//	printf("请输入两个数：");
+//	scanf("%d %d", &a, &b);
+//	printf("%d\n", p(a, b));
+//}
+//int main()
+//{
+//	int input = 0;
+//	int(*p1[5])(int, int) = { NULL,add,sub,mul,dev };
+//	do {
+//		manu();
+//		printf("请选择：");
+//		scanf("%d", &input);
+//		switch (input)
+//		{
+//		case 1:
+//			//cala(&p1,input);
+//			cala1(add);
+//			break;
+//		case 2:
+//			//cala(&p1,input);
+//			cala1(sub);
+//			break;
+//		case 3:
+//			//cala(&p1,input);
+//			cala1(mul);
+//			break;
+//		case 4:
+//			//cala(&p1,input);
+//			cala1(dev);
+//			break;
+//		case 0:
+//			printf("退出程序\n");
+//			break;
+//		default:
+//			printf("输入错误，请再次选择\n");
+//		}
+//	} while (input);
+//
+//	return 0;
+//}
+//void manu()
+//{
+//	printf("********************************\n");
+//	printf("******1、加法    2、减法********\n");
+//	printf("********************************\n"); 
+//	printf("******3、乘法    4、除法********\n");
+//	printf("********************************\n");
+//	printf("**********0、退出 **************\n");
+//
+//}
+//int add(int a, int b)
+//{
+//	return (a + b);
+//}
+//int sub(int a,int b)
+//{
+//	return (a - b);
+//}
+//int mul(int a, int b)
+//{
+//	return a * b;
+//}
+//int dev(int a, int b)
+//{
+//	return a/b;
+//}
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	int input = 0;
+//	int(*p[5])(int, int) = {NULL,add,sub,mul,dev};
+//	int(*(*p1)[5])(int, int) = &p;
+//	do {
+//		manu();
+//		printf("请选择：");
+//    	scanf("%d", &input);
+//		if (input>0 && input <= 4)
+//		{
+//			printf("请输入两个数：");
+//			scanf("%d %d", &a, &b);
+//			printf("%d\n",(*p1)[input](a, b));
+//		}
+//		else if (input == 0)
+//		{
+//			printf("退出程序\n");
+//			break;
+//		}
+//		else
+//		{
+//			printf("输入错误，请再次选择\n");
+//		}
+//		
+//	} while (input);
+//	
+//	return 0;
+//}
+//int factorial(int*a)
+//{
+//	int i, j;
+//	int sum, temp;
+//	for (i = 1,sum=0; i <= *a; i++)
+//	{
+//		for (j = 1,temp=1; j <= i; j++)
+//		{
+//			temp *=j;	
+//		}
+//		sum += temp;
+//	}
+//	return sum;
+//	
+//}
+//int main()
+//{
+//	int a=0;
+//	int(*p)(int*) = factorial;
+//	int(*p1)(int*) = &factorial;
+//	scanf("%d", &a);
+//	printf("%p %p\n", factorial, &factorial);
+//	printf("%p %p\n", *p, *p1);
+//	printf("%p %p\n", p, p1);
+//	printf("%d\n", factorial(&a));
+//	printf("%d %d\n", (*p)(&a),(*p1)(&a));
+//	printf("%d %d\n", p(&a),p1(&a));
+//	return 0;
+//}
+//int main()
+//{
+//	int a[] = { 11,22,55,7 };
+//	int b[] = { 1,7,9 };
+//	int* p[2] = { a,b };
+//	printf("%d %d ", *p[0], *p[1]);
+//	return 0;
+//}
+//int main()
+//{
+//	int shu[3] = { 11,33,77 };
+//	int* p = shu;
+//	int(*p1)[3] = &shu;
+//	printf("%d\n", *p);
+//	printf("%d ", **p1);
+//	return 0;
+//}
+//int main()
+//{
+//	int i = 0;
+//	char shu[][7] = { "today","is","sunday" };
+//	const char* p[] = { "today","is","sunday" };
+//	for (i = 0; i < 3; i++)
+//	{
+//		printf("%c %c ",*p[i],*(*(p+i)));
+//	}	
+//	puts("\n");
+//	for (i = 0; i < 3; i++)
+//	{
+//		printf("%c", *shu[i]);
+//	}
+//	return 0;
+//}
+
+
+	
+//void print(int*(*p)[2])
+//{
+//	int i = 0;
+//	int j = 0;
+//	printf("%d %d\n", ***p, ***(p + 1));
+//	puts("\n");
+//	for (i = 0; i < 2; i++)
+//	{
+//		for (j = 0; j < 2; j++)
+//		{
+//			printf("%d %d ", **(*(p + i) + j), **(p[i] + j));
+//		}
+//		puts("\n");
+//	}
+//}
+//int main()
+//{
+//	int shu11[4] = { 11,33,77,99 };
+//	int* shu[2][2] = { {&shu11[0],&shu11[1]},{&shu11[2],&shu11[3]} };
+//		print(shu);
+//		return 0;
+//}
+//int main()
+//{
+//	int shu[5] = { 1,2,3,4,5 };
+//	int* p=shu;
+//	while(p<&shu[5])	
+//	{
+//		*p++ = 9;   
+//	}
+//	for (p = &shu[0]; p< &shu[5];p++)
+//	{
+//	printf("%d ", *p);
+//	}
+//	return 0;
+//}
+// char* copy(char* a,const char* b)
+//{
+//	 char* c = NULL;
+//	 assert(a != NULL);
+//	 assert(b != NULL);//记得加#include <cassert>
+//	 //assert(c != NULL);
+//	// 该效果等同于：
+//	 if (c==NULL)
+//	 {
+//		 puts("a=NULL");
+//		 abort();
+//	 }
+//	
+//	 char* adress = a;
+//	while (*a++ = *b++)
+//	{
+//		;
+//	}//当跳出循环时，指针a指向的位置是'\0'的位置，
+//	return adress;//所以要创建一个指针变量储存首地址
+//}
+//int main()
+//{
+//	char shu[] = "what day is it today?";
+//	char shu11[] = "tomorrow";
+//	printf("%s %s\n", copy(shu, shu11), shu11);
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	char* p =(char*)malloc(12 * sizeof(int));
+//	if (p != NULL)
+//	{
+//		strcpy(p, "sunday");
+//		*p = 'r';
+//		printf("%s", p);
+//	}
+//	
+//	return 0;
+
+
+
+	//char a = 'r';
+	//char b = 'y';
+	//const char* p1 = &a;
+	//p1 = &b;
+	//printf("%c",*p1);
+	/*const char* p = "sunday";
+	p = "we";
+	printf("%s", p);*/
+
+	
+
+//}
+//void print(int(*p)[5], int x, int y)
+//{
+//	int i;
+//	int j=0;
+//	printf("%d %d %d", **p, **(p + 1), **(p + 2));
+//	//for (i = 0; i < x; i++)
+//	//{
+//	//	for (j = 0; j < y; j++)
+//	//	{
+//	//		//printf("%d  %d  %d  %d\n", *(*(p + i) + j),(*(p+i))[j],*(p[i]+j),p[i][j]);
+//	//	
+//	//	}
+//	//}
+//}
+//int main()
+//{
+//	int shu[3][5] = { {1,2,3,4,5},{6,7,8,9,0},{1,1,1,5,7} };
+//	print(shu,3,5);
+//	return 0;
+//}
+////int main()
+//{
+//	int shu[4] = { 0,2,3,4 };
+//	int(*p)[4] = &shu;
+//	printf("%d %d %d\n", (*p)[2],*(*p+2),**p);
+//	/*但一般都这样写：*/
+//	/*int* p = shu;*/
+//	/*printf("%d %d\n",*(p+2),p[2]);*/
+//	return 0;
+//}
+//int main()
+//{
+//	/*char shu1[] = "we!";
+//	char* shu[2] = { &shu1[0],&shu1[1]};*/
+//	char a = 'w';
+//	char b = 'e';
+//	char* shu[2] = { &a,&b };
+//	char* (*p)[2] = &shu;
+//	printf("%c %c\n", *(*p)[0], *(*p)[1]);
+//	/*char* (*p)[2] = &shu;
+//	printf("%c %c\n", *(*p)[0], *(*p)[1]);*/
+//	return 0;
+//}
+
+//int main()
+//{
+//	char str1[] = "hello bit.";
+//	char str2[] = "hello bit.";
+//	const char* str3 = "hello bit.";
+//	const char* str4 = "hello bit.";
+//	if (str1 == str2)
+//		printf("str1 and str2 are same\n");
+//	else
+//		printf("str1 and str2 are not same\n");
+//
+//	if (str3 == str4)
+//		printf("str3 and str4 are same\n");
+//	else
+//		printf("str3 and str4 are not same\n");
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	int i = 123;
+//	int* a= &i;
+//	int** b = &a;//int**b创建的是二级指针只能用一级指针赋值
+//	int*** c = &b;//和上面一样原理，该变量只能存放二级指针地址
+//	printf("%d %d %d\n", *a, **b, ***c);
+//	printf("%p %p %p\n", a, *b,**c);
+//	printf("%p %p\n", b, *c);
+//}
+//int main()
+//{
+//	int shu[5] = { 1,2,3,4,5};
+//	int* p;
+//	for (p = &shu[5]; p > &shu[0];)
+//	{
+//		*--p = 0;
+//		
+//		printf("%d\n", *p);
+//	}
+//	return 0;
+//}
+//int count(char* shu)
+//{
+	//char* start = shu;
+	/*char* end = shu;
+	while (*end != '\0')*/
+
+	/*char* start = shu;
+	char* end = shu;
+	while (*shu++ != '\0')
+		end++;
+	return end - shu;*///start;
+	//该效果等同于如下代码：
+	//int shu1[16]={0};
+	//printf("%d",&shu[15]-&shu[0]);
+//}
+//int main()
+//{
+//	char shu[] = "today is sunday!";
+//	int a = count(shu);
+//	int shu1[4] = { 0,12,22,11 };
+//	printf("%d\n",shu1[3]-shu1[2]);
+//	printf("%d\n", a);
+//	return 0;
+//}
+//int main()
+//{
+//	int n = 0x11223344;
+//	char* pc = (char*)&n;
+//	int* pi = &n;
+//	*pc = 0;   //重点在调试的过程中观察内存的变化。
+//	*pi = 0;   //重点在调试的过程中观察内存的变化。
+//	return 0;
+//}
+
+//int my_strlen(char* s)
+//{
+//	char* p = s;
+//	while (*p != '\0')
+//		p++;
+//	
+//	return p - s;
+//}
+//int main()
+//{
+//	char b[12] ="hhhhgggggR";
+//	char* p = b;
+//	char* s = b;
+//	int a= my_strlen(s);
+//	printf("%d\n", a);
+//	printf("%p %p %p %p\n",&b[9], &p[9], p + 9, b + 9);//打印结果相同
+//	printf("%c %c %c %c", b[9],p[9],*(p+9),*(b+9));//打印结果相同
+	//我们可以知道除了用数组访问还可以用指针去访问元素
+	//如上代码为例
+	//&b[i]=p+i  所表示的地址相同且与i一一对应
+	//b[i]=p[i]=*(p+i)  所表示的数组元素相同且i一一对应
+//}
+//int main()
+//{
+//	int n = 9;
+//	float* pFloat = (float*)&n;
+//	printf("n的值为：%d\n", n);
+//	printf("*pFloat的值为：%f\n", *pFloat);
+//	*pFloat = 9.0;
+//	printf("num的值为：%d\n", n);
+//	printf("*pFloat的值为：%f\n", *pFloat);
+//	return 0;
+//}
 //int* remind()
 //{
 //	int a = 1;//局部变量，一旦出了函数将会销毁该变量
